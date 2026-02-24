00a4a6f00268e2572156e34552298bb808655576
Oscar <orromerop@hotmail.com>
2026-02-23 19:20:59 -0600

    feat: prueba flujo con pull request

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <h1 className="text-4x1 font-bold">Mi taller MVP</h1>
       <h1 className="text-4x1 font-bold">Version DEV</h1>
+      <h1 className="text-4x1 font-bold">V 1.1</h1>
       <h2 className="text-2x1 font-semibold mb-4">La solución inteligente para talleres que quieren profesionalizar su gestión y llevar el control total de sus servicios y clientes.</h2>
       <h3 className="text-2x1 font-semibold mb-4">Catálogo de Servicios</h3>
 
